703bfdb36e14b0c7fe383372f27abb11385e45f1
josephclinthon <josephclinthon@gmail.com>
2026-09-07 14:09:05 -0500

    v2026_09_07

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url)
-
-    if (url.pathname === "/data.json") {
-      return env.ASSETS.fetch(request)
-    }
-
-    return new Response("Not Found", {
-      status: 404
-    })
+    return env.ASSETS.fetch(request)
   }
 }
