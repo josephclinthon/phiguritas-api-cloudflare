7f388adf2344c908ec25f3140328d3bf81ecbe3d
josephclinthon <josephclinthon@gmail.com>
2026-09-07 11:00:04 -0500

    prueba 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url)
 
-    if (url.pathname === "/data.json") {
+    if (url.pathname === "/albums.json") {
       return env.ASSETS.fetch(request)
     }
 
